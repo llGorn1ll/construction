675de8ffba812eb71fab4002403c139a556b3418
llGorn1ll <gorganikita02@gmail.com>
2025-07-17 22:08:25 +0600

    Update db.php

--- vendor/db.php
+++ vendor/db.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-$link = new mysqli('localhost', 'v908115t_wedding', 'Wedding123)', 'v908115t_wedding');
+$link = new mysqli('localhost', 'root', 'root)', 'construction');
 $link->set_charset('utf8mb4');
 ?>
